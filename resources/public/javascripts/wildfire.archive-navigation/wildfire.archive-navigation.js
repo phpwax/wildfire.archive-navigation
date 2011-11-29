eaa9e5c384dbbfcaf8050ed9ce54d4bab0d3c502
charlesmarshall <cm56marshall@googlemail.com>
2011-11-29 17:09:45 +0000

    js file to do the sliding

---
+++ resources/public/javascripts/wildfire.archive-navigation/wildfire.archive-navigation.js
@@ -0,0 +1,14 @@
+jQuery(document).ready(function(){
+  
+  jQuery(".archive_nav a.year").bind("click", function(e){
+    e.preventDefault();
+    e.stopPropagation();
+    jQuery(this).parent().children("ul.nav:eq(0)").slideToggle();    
+  });
+  jQuery(".archive_nav a.month").bind("click", function(e){
+    e.preventDefault();
+    e.stopPropagation();
+    jQuery(this).parent().children("ul.nav:eq(0)").slideToggle();    
+  });
+  
+});
