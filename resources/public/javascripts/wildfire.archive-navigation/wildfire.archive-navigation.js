b83be88d3e6d6ed4ce77a68fe7d2b89c94076e6a
Adam Preece <adam.p@oneblackbear.com>
2013-06-12 15:16:37 +0100

    add class to style the open and closed states

--- resources/public/javascripts/wildfire.archive-navigation/wildfire.archive-navigation.js
+++ resources/public/javascripts/wildfire.archive-navigation/wildfire.archive-navigation.js
@@ -3,12 +3,12 @@ jQuery(document).ready(function(){
   jQuery(".archive_nav a.year").bind("click", function(e){
     e.preventDefault();
     e.stopPropagation();
-    jQuery(this).parent().children("ul.nav:eq(0)").slideToggle();    
+    jQuery(this).parent().toggleClass("open-nav").children("ul.nav:eq(0)").slideToggle();    
   });
   jQuery(".archive_nav a.month").bind("click", function(e){
     e.preventDefault();
     e.stopPropagation();
-    jQuery(this).parent().children("ul.nav:eq(0)").slideToggle();    
+    jQuery(this).parent().toggleClass("open-nav").children("ul.nav:eq(0)").slideToggle();    
   });
   
   jQuery(".archive_nav li:not(.in_stack) a.year, .archive_nav li:not(.in_stack) a.month").trigger("click");
