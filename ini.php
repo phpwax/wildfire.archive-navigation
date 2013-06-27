a82e8ea518d1d461fd1c5d161488cf383808cc4f
Charles Marshall <charles@oneblackbear.com>
2013-06-27 11:27:42 +0100

    register assets

--- ini.php
+++ ini.php
@@ -1,3 +1,5 @@
 <?
+AutoLoader::register_assets("javascripts/wildfire.archive-navigation",__DIR__."/resources/public/javascripts/wildfire.archive-navigation", "/*.js");
+AutoLoader::register_assets("stylesheets/wildfire.archive-navigation",__DIR__."/resources/public/stylesheets/wildfire.archive-navigation", "/*.css");
 AutoLoader::add_plugin_setup_script(__DIR__."/setup.php");
 ?>
